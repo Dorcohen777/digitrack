2bd8f300dbe484338f06d838dc31568305efc48f
Dorcohen777 <dorcohen1337@gmail.com>
2024-01-13 17:25:22 +0200

    hero section building

--- frontend/src/pages/header.jsx
+++ frontend/src/pages/header.jsx
@@ -2,14 +2,18 @@ import { Link } from 'react-router-dom'
 
 export function AppHeader() {
     return (
-        <nav className='app-nav-container flex  align-center full'>
-            <h2 className='site-logo'>DigiTrack</h2>
-            <ul className='nav-list'>
-                <li>
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/aboutus'}>About Us</Link>
-                </li>
-            </ul>
+        <nav className='full'>
+            <div className='app-nav-container main-layout'>
+                <div className='app-nav-inner-container flex align-center'>
+                    <h2 className='site-logo'>DigiTrack</h2>
+                    <ul className='nav-list'>
+                        <li>
+                            <Link to={'/'}>Home</Link>
+                            <Link to={'/aboutus'}>About Us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import heroImg from '../assets/img/hero-img.jpg'
+
 // import { useDispatch, useSelector } from 'react-redux'
 
 
 export function HomePage() {
-   
+
     return (
-        <section>
-            <h1>home page</h1>
+        <section className='hero-main-container'>
+            <div className='hero-inner-container flex align-center justify-center'>
+                <div className='hero-text-container'>
+                    <h2 className='hero-title'>Track Smart, Market Sharp!</h2>
+                    <h1>DigiTrack</h1>
+                    <p>Effortlessly track, budget, and optimize marketing expenses. <br/>Maximize ROI with ease!</p>
+                </div>
+                <img src={heroImg} className='hero-img' />
+            </div>
+
         </section>
     )
 }
