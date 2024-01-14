88c35ff94d09fc09f8365830e20040d9f599871b
Dorcohen777 <dorcohen1337@gmail.com>
2024-01-14 09:41:00 +0200

    hero section style improve

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -8,18 +8,18 @@ export function HomePage() {
 
     return (
         <section className='hero-main-container'>
-            <div className='hero-inner-container flex align-center justify-center'>
+            <div className='hero-inner-container flex align-center '>
                 <div className='hero-text-container'>
-                    <h2 className='hero-title'>Track Smart, Market Sharp!</h2>
-                    <h1>DigiTrack</h1>
-                    <p>Effortlessly track, budget, and optimize marketing expenses. <br />Maximize ROI with ease!</p>
+                    <h2 className='hero-title fs28'>Track Smart, Market Sharp!</h2>
+                    <h1 className='fs50'>DigiTrack</h1>
+                    <p className='fs24'>Effortlessly track, budget, and optimize marketing expenses. <br />Maximize ROI with ease!</p>
+                    <button>Try demo</button>
                 </div>
                 <div>
                     <img src={heroImg} className='hero-img' />
-                    
+
                 </div>
             </div>
-
         </section>
     )
 }
